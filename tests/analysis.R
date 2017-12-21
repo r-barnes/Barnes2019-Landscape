256445b4e32665b5b717c2013b8662a011e42883
Richard Barnes <rbarnes@umn.edu>
2017-12-21 11:54:46 -0600

    Update analysis

--- tests/analysis.R
+++ tests/analysis.R
@@ -5,7 +5,7 @@ library(reshape2)
 library(plyr)
 library(ggplot2)
 
-df <- read.table('benchmarks.tbl', header=TRUE)
+df <- read.table('benchmarks.tbl', header=TRUE, sep="|")
 
 df <- df %>% select(-Prog,-Rep)                                     %>% 
              mutate(Algorithm=gsub('FastScape_','',Algorithm))
@@ -17,4 +17,4 @@ df <- melt(df, id.vars=c("Algorithm", "Size", "Steps"))
 df <- ddply(df, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable)
 df$variable <- factor(df$variable, levels = sort(as.character(unique(df$variable))))
 
-ggplot(df, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size)
+ggplot(df, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_y")
