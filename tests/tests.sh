e29c755922fc7ecd2b7941f05f9bfa04ff5a917c
Richard Barnes <rbarnes@umn.edu>
2017-12-21 12:42:16 -0600

    Update tests for serial comparison

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+host=$(hostname)
+
 #Edge length of a dataset. Number of cells is the square of this value.
 sizes=( 100 700 1000 7000 10000 ) 
 sizes=( 200 500 1000 )
@@ -14,20 +16,23 @@ steps=120
 exe_prefix=../
 progs=( fastscape_BW.exe fastscape_BW+P.exe fastscape_RB.exe fastscape_RB+P.exe fastscape_RB+PQ.exe )
 
-arraylength=${#array[@]}
 
-# use for loop to read all values and indexes
+if [ ! -f z_serial_comparison.dat ]; then
+  progs=( fastscape_BW.exe fastscape_RB.exe )
+  sizes=( 100 700 1000 7000 10000 ) 
+  reps=( 3 3 3 3 3 )
+  for prog in "${progs[@]}"; do
+  for (( s=0;   s<${#sizes[@]}; s++ )); do
+  for (( rep=0; rep<${reps[s]}; rep++ )); do
+    size=${sizes[s]}
+    echo "# Prog  = $prog"
+    echo "m Size  = $size"
+    echo "m Steps = $steps"
+    echo "m Rep   = $rep"
+    echo "H host  = $host"
 
-for prog in "${progs[@]}"; do
-for (( s=0;   s<${#sizes[@]}; s++ )); do
-for (( rep=0; rep<${reps[s]}; rep++ )); do
-  size=${sizes[s]}
-  echo "# Prog  = $prog"
-  echo "m Size  = $size"
-  echo "m Steps = $steps"
-  echo "m Rep   = $rep"
-
-  eval "$exe_prefix$prog $size $steps"
-done
-done
-done
+    eval "$exe_prefix$prog $size $steps"
+  done
+  done
+  done > >(tee -i z_serial_comparison.dat)
+fi
