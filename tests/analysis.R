bf7bb736eb2c79db1f11d1dbc42f75fa06be0838
Richard Barnes <rbarnes@umn.edu>
2017-12-23 16:02:38 -0600

    Plot scaling

--- tests/analysis.R
+++ tests/analysis.R
@@ -18,10 +18,14 @@ df <- melt(df, id.vars=c("Algorithm", "Size", "Steps"))
 
 df <- ddply(df, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable) %>% mutate(mean=mean/1e6, sd=sd/1e6)
 df$variable <- factor(df$variable, levels = sort(as.character(unique(df$variable))))
+#df$Size     <- factor(df$Size)
 
 df %>% group_by(variable,Size,Steps) %>% summarise(meandiff=max(mean)-min(mean), sddiff=max(sd)-min(sd), pmean=(max(mean)-min(mean))/max(mean))
 
 ggplot(df, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_y")
+
+temp <- df %>% filter(variable=='Overall')
+ggplot(temp, aes(x=Size, y=mean, color=Algorithm)) + geom_point() + geom_smooth(method = "lm", se=FALSE) + theme(axis.text.x=element_text(angle=45, hjust=1)) + scale_y_log10()
 
 
 
