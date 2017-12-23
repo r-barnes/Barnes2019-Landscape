493ad98d51cf8409217d906e653879a5c9aa61d6
Richard Barnes <rbarnes@umn.edu>
2017-12-23 12:01:22 -0600

    Clean up analysis

--- tests/analysis.R
+++ tests/analysis.R
@@ -1,20 +1,29 @@
-#./d.make_tabular -s '#' /z/out > ./benchmarks.tbl
+#cat z_serial_comparison.dat | ./d.make_tabular -s '#' | grep -v "^---" > z_serial_benchmarks.tbl
 
+library(plyr)
 library(dplyr)
 library(reshape2)
-library(plyr)
 library(ggplot2)
 
-df <- read.table('benchmarks.tbl', header=TRUE, sep="|")
 
-df <- df %>% select(-Prog,-Rep)                                     %>% 
+
+
+
+df <- read.table('z_serial_benchmarks.tbl', header=TRUE, sep="|")
+
+df <- df %>% select(-Rep,-host,-hash)                                     %>% 
              mutate(Algorithm=gsub('FastScape_','',Algorithm))
-
-#df <- df %>% group_by(Algorithm,Size) %>% summarise_all(.funs = c(mean="mean", sd="sd"))
 
 df <- melt(df, id.vars=c("Algorithm", "Size", "Steps"))
 
-df <- ddply(df, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable)
+df <- ddply(df, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable) %>% mutate(mean=mean/1e6, sd=sd/1e6)
 df$variable <- factor(df$variable, levels = sort(as.character(unique(df$variable))))
 
+df %>% group_by(variable,Size,Steps) %>% summarise(meandiff=max(mean)-min(mean), sddiff=max(sd)-min(sd), pmean=(max(mean)-min(mean))/max(mean))
+
 ggplot(df, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_y")
+
+
+
+
+df %>% group_by(variable,Size,Steps) %>% summarise(meandiff=max(mean)-min(mean), sddiff=max(sd)-min(sd), pmean=(max(mean)-min(mean))/max(mean))
