ecabb19eb541d98395de74c1ccfa93382ae63c20
Richard Barnes <rbarnes@umn.edu>
2018-02-08 00:56:46 -0800

    Allocate additional memory for threads' queue width

--- fastscape_RB+PQ.cpp
+++ fastscape_RB+PQ.cpp
@@ -398,7 +398,7 @@ class FastScape_RBPQ {
       ndon[i] = 0;
 
     //TODO: Make smaller, explain max
-    stack_width = std::max(100,2*size/omp_get_max_threads()); //Number of stack entries available to each thread
+    stack_width = std::max(100,4*size/omp_get_max_threads()); //Number of stack entries available to each thread
     level_width = std::max(100,size/omp_get_max_threads());   //Number of level entries available to each thread
 
     #pragma omp parallel
