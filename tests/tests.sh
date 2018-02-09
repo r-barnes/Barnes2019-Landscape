994de2507eb2e4fca9d47be03213cb61b989b3d4
Richard Barnes <rbarnes@umn.edu>
2018-02-08 17:01:25 -0800

    Add additional tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -2,24 +2,21 @@
 
 host=$(hostname)
 
-#Edge length of a dataset. Number of cells is the square of this value.
-sizes=( 100 700 1000 7000 10000 ) 
-sizes=( 200 500 1000 )
-
-#Number of repetitions for each dataset size. Statistical significance!
-reps=( 10 10 10 10 10 )           
-reps=( 2 2 2 2 2 )   
 
 steps=120        
 
 #Programs to run
 exe_prefix=../
-progs=( fastscape_BW.exe fastscape_BW+P.exe fastscape_RB.exe fastscape_RB+P.exe fastscape_RB+PQ.exe )
 
+if [ ! -f "z_parallel_sep_thread_$TESTSYSTEM.dat" ]; then
+  echo "RUNNING PARALLEL IMPROVED TESTS"
 
-if [ ! -f z_serial_comparison.dat ]; then
-  progs=( fastscape_BW.exe fastscape_RB.exe )
+  progs=( fastscape_RB+PQ.exe )
+
+  #Edge length of a dataset. Number of cells is the square of this value.
   sizes=( 100 700 1000 7000 10000 ) 
+
+  #Number of repetitions for each dataset size. Statistical significance!
   reps=( 3 3 3 3 3 )
   for prog in "${progs[@]}"; do
   for (( s=0;   s<${#sizes[@]}; s++ )); do
@@ -31,14 +28,25 @@ if [ ! -f z_serial_comparison.dat ]; then
     echo "m Rep   = $rep"
     echo "H host  = $host"
 
-    echo "R $exe_prefix$prog $size $steps out_${prog}_${size}_${steps}_${rep}.dem 123"
-    eval "$exe_prefix$prog $size $steps out_${prog}_${size}_${steps}_${rep}.dem 123"
+    echo "R $exe_prefix$prog $size $steps out_simp_parallel_${prog}_${size}_${steps}_${rep}_${TESTSYSTEM}.dem 123"
+    eval "$exe_prefix$prog $size $steps out_simp_parallel_${prog}_${size}_${steps}_${rep}_${TESTSYSTEM}.dem 123"
   done
   done
-  done > >(tee -i z_serial_comparison.dat)
+  done > >(tee -i "z_parallel_sep_thread_$TESTSYSTEM.dat")
+fi
 
-  sizes=( 100 500 1000 ) 
-  reps=( 100 50 20 )
+exit 0
+
+if [ ! -f "z_parallel_improved_$TESTSYSTEM.dat" ]; then
+  echo "RUNNING PARALLEL IMPROVED TESTS"
+
+  progs=( fastscape_BW+PI.exe fastscape_RB+PI.exe )
+
+  #Edge length of a dataset. Number of cells is the square of this value.
+  sizes=( 100 700 1000 7000 10000 ) 
+
+  #Number of repetitions for each dataset size. Statistical significance!
+  reps=( 3 3 3 3 3 )
   for prog in "${progs[@]}"; do
   for (( s=0;   s<${#sizes[@]}; s++ )); do
   for (( rep=0; rep<${reps[s]}; rep++ )); do
@@ -49,9 +57,67 @@ if [ ! -f z_serial_comparison.dat ]; then
     echo "m Rep   = $rep"
     echo "H host  = $host"
 
-    echo "R $exe_prefix$prog $size $steps out_${prog}_${size}_${steps}_${rep}.dem 0"
-    eval "$exe_prefix$prog $size $steps out_${prog}_${size}_${steps}_${rep}.dem 0"
+    echo "R $exe_prefix$prog $size $steps out_simp_parallel_${prog}_${size}_${steps}_${rep}_${TESTSYSTEM}.dem 123"
+    eval "$exe_prefix$prog $size $steps out_simp_parallel_${prog}_${size}_${steps}_${rep}_${TESTSYSTEM}.dem 123"
   done
   done
-  done > >(tee -a -i z_serial_comparison.dat) 
+  done > >(tee -i "z_parallel_improved_$TESTSYSTEM.dat")
+fi
+
+
+
+
+if [ ! -f "z_serial_comparison_$TESTSYSTEM.dat" ]; then
+  echo "RUNNING SERIAL TESTS"
+
+  progs=( fastscape_BW.exe fastscape_RB.exe )
+
+  #Edge length of a dataset. Number of cells is the square of this value.
+  sizes=( 100 700 1000 7000 10000 ) 
+
+  #Number of repetitions for each dataset size. Statistical significance!
+  reps=( 3 3 3 3 3 )
+  for prog in "${progs[@]}"; do
+  for (( s=0;   s<${#sizes[@]}; s++ )); do
+  for (( rep=0; rep<${reps[s]}; rep++ )); do
+    size=${sizes[s]}
+    echo "# Prog  = $prog"
+    echo "m Size  = $size"
+    echo "m Steps = $steps"
+    echo "m Rep   = $rep"
+    echo "H host  = $host"
+
+    echo "R $exe_prefix$prog $size $steps out_serial_${prog}_${size}_${steps}_${rep}_${TESTSYSTEM}.dem 123"
+    eval "$exe_prefix$prog $size $steps out_serial_${prog}_${size}_${steps}_${rep}_${TESTSYSTEM}.dem 123"
+  done
+  done
+  done > >(tee -i "z_serial_comparison_$TESTSYSTEM.dat")
+fi
+
+
+if [ ! -f "z_simple_parallel_$TESTSYSTEM.dat" ]; then
+  echo "RUNNING SIMPLE PARALLEL TESTS"
+
+  progs=( fastscape_BW+P.exe fastscape_RB+P.exe )
+
+  #Edge length of a dataset. Number of cells is the square of this value.
+  sizes=( 100 700 1000 7000 10000 ) 
+
+  #Number of repetitions for each dataset size. Statistical significance!
+  reps=( 3 3 3 3 3 )
+  for prog in "${progs[@]}"; do
+  for (( s=0;   s<${#sizes[@]}; s++ )); do
+  for (( rep=0; rep<${reps[s]}; rep++ )); do
+    size=${sizes[s]}
+    echo "# Prog  = $prog"
+    echo "m Size  = $size"
+    echo "m Steps = $steps"
+    echo "m Rep   = $rep"
+    echo "H host  = $host"
+
+    echo "R $exe_prefix$prog $size $steps out_simp_parallel_${prog}_${size}_${steps}_${rep}_${TESTSYSTEM}.dem 123"
+    eval "$exe_prefix$prog $size $steps out_simp_parallel_${prog}_${size}_${steps}_${rep}_${TESTSYSTEM}.dem 123"
+  done
+  done
+  done > >(tee -i "z_simple_parallel_$TESTSYSTEM.dat")
 fi
