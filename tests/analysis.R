9d9555d52955f2d53b6510e89b30e516b48aba17
Richard Barnes <rbarnes@umn.edu>
2018-02-10 02:45:04 -0800

    Add GPU scaling tests

--- tests/analysis.R
+++ tests/analysis.R
@@ -167,3 +167,61 @@ erosion_per<-widedf %>% select(Algorithm,Steps,Size,Overall,Step7_Erosion) %>% m
 mean(erosion_per$per)
 
 
+
+
+
+#################################
+#*+GPU.exe : GPU Scaling
+#################################
+
+df <- read.table('z_gpu_scaling_summitdev.tbl', header=TRUE, sep="|")
+
+df <- df %>% select(-Rep,-host,-hash,-Random_seed)                                     %>% 
+             mutate(Algorithm=gsub('FastScape_','',Algorithm))
+
+df <- melt(df, id.vars=c("Algorithm", "Size", "Steps")) %>%
+      mutate(variable=gsub('Step(.)','Step \\1:',gsub('_',' ',variable)))
+
+df <- ddply(df, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable) %>% mutate(mean=mean/1e6, sd=sd/1e6)
+df$variable <- factor(df$variable, levels = rev(sort(as.character(unique(df$variable)))))
+
+#ggplot(df, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_x") + coord_flip() + xlab("") + ylab("Wall-time (s)") + scale_fill_manual(values=c('#00BFC4'), guide = guide_legend()) + theme(legend.position="bottom")
+temp <- df %>% filter(variable=="Overall")
+
+p<-ggplot(temp, aes(x=Size**2, y=mean, color=Algorithm)) + 
+     #geom_rect(aes(xmin = 0,    xmax = 400, ymin = min(temp$mean), ymax = Inf), fill = "pink", alpha = 0.01, color=NA) +
+     #geom_rect(aes(xmin = 400,  xmax = 1000, ymin = min(temp$mean), ymax = Inf), fill = "pink", alpha = 0.01, color=NA) +
+     #geom_rect(aes(xmin = 1000, xmax = 2500, ymin = min(temp$mean), ymax = Inf), fill = "pink", alpha = 0.01, color=NA) +
+     geom_vline(xintercept=400**2)                                              +
+     geom_vline(xintercept=1000**2)                                             +
+     geom_vline(xintercept=2500**2)                                             +
+     geom_text(aes(x=200**2,  y=80, label="A"), color="black")                  +
+     geom_text(aes(x=750**2,  y=80, label="B"), color="black")                  +
+     geom_text(aes(x=1750**2, y=80, label="C"), color="black")                  +
+     geom_text(aes(x=6500**2, y=80, label="D"), color="black")                  +
+     geom_point()                                                               + 
+     geom_line()                                                                + 
+     scale_x_log10()                                                            + 
+     scale_y_log10()                                                            + 
+     annotation_logticks()                                                      + 
+     theme(axis.text.x=element_text(angle=45, hjust=1))                         + 
+     xlab("Cells")                                                              + 
+     ylab("Wall-time (s)")                                                      + 
+     scale_color_manual(values=c('#00BFC4'), guide = guide_legend())            + 
+     theme_grey(base_size=6)                                                    + 
+     theme(legend.position="bottom", legend.margin=margin(t = -0.3, unit='cm')) + 
+     guides(fill = guide_legend(keywidth=0.4, keyheight=0.4))                   
+
+ggsave(filename="img_gpu_scaling_summitdev.pdf", plot=p, units="in", width=3, height=1.5)
+
+lmdat <- temp %>% filter(0<=Size & Size<=400)
+lm(log10(lmdat$mean) ~ log10(lmdat$Size**2))
+
+lmdat <- temp %>% filter(400<=Size & Size<=1000)
+lm(log10(lmdat$mean) ~ log10(lmdat$Size**2))
+
+lmdat <- temp %>% filter(1000<=Size & Size<=2500)
+lm(log10(lmdat$mean) ~ log10(lmdat$Size**2))
+
+lmdat <- temp %>% filter(2500<=Size)
+lm(log10(lmdat$mean) ~ log10(lmdat$Size**2))
--- tests/tests.sh
+++ tests/tests.sh
@@ -2,13 +2,39 @@
 
 host=$(hostname)
 
-
 steps=120        
 
 #Programs to run
 exe_prefix=../
 
 if [ "$TESTSYSTEM" == "summitdev" ]; then
+
+  if [ ! -f "z_gpu_scaling_$TESTSYSTEM.dat" ]; then
+    echo "RUNNING GPU TESTS"
+
+    progs=( fastscape_RB+GPU.exe )
+
+    #Edge length of a dataset. Number of cells is the square of this value.
+    sizes=( 100 200 400 800 1000 1500 2000 2500 3000 3500 4000 4500 5000 5500 6000 6500 7000 7500 8000 8500 9000 9500 10000 10500 11000 11500 12000 12500 13000 13500 14000 14500 15000 15500 16000 16500 17000 17500 18000 18500 19000 19500 20000 ) 
+
+    #Number of repetitions for each dataset size. Statistical significance!
+    reps=(    1   1   1   1    1    1    1    1    1    1    1    1    1    1    1    1    1    1    1    1    1    1     1     1     1     1     1     1     1     1     1     1     1     1     1     1     1     1     1     1     1     1     1 )
+    for prog in "${progs[@]}"; do
+    for (( s=0;   s<${#sizes[@]}; s++ )); do
+    for (( rep=0; rep<${reps[s]}; rep++ )); do
+      size=${sizes[s]}
+      echo "# Prog  = $prog"
+      echo "m Size  = $size"
+      echo "m Steps = $steps"
+      echo "m Rep   = $rep"
+      echo "H host  = $host"
+
+      echo "R $exe_prefix$prog $size $steps out_simp_parallel_${prog}_${size}_${steps}_${rep}_${TESTSYSTEM}.dem 123"
+      eval "$exe_prefix$prog $size $steps out_simp_parallel_${prog}_${size}_${steps}_${rep}_${TESTSYSTEM}.dem 123"
+    done
+    done
+    done > >(tee -i "z_gpu_scaling_$TESTSYSTEM.dat")
+  fi
 
   if [ ! -f "z_gpu_$TESTSYSTEM.dat" ]; then
     echo "RUNNING GPU TESTS"
@@ -36,12 +62,7 @@ if [ "$TESTSYSTEM" == "summitdev" ]; then
     done
     done > >(tee -i "z_gpu_$TESTSYSTEM.dat")
   fi
-
 fi
-
-exit 0
-
-
 
 
 
@@ -74,8 +95,6 @@ fi
 
 
 
-
-
 if [ ! -f "z_parallel_improved_$TESTSYSTEM.dat" ]; then
   echo "RUNNING PARALLEL IMPROVED TESTS"
 
@@ -105,7 +124,6 @@ fi
 
 
 
-
 if [ ! -f "z_serial_comparison_$TESTSYSTEM.dat" ]; then
   echo "RUNNING SERIAL TESTS"
 
@@ -132,6 +150,7 @@ if [ ! -f "z_serial_comparison_$TESTSYSTEM.dat" ]; then
   done
   done > >(tee -i "z_serial_comparison_$TESTSYSTEM.dat")
 fi
+
 
 
 if [ ! -f "z_simple_parallel_$TESTSYSTEM.dat" ]; then
