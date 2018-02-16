1e3e162b81e65d3b3598c960634334342d3cf21f
Richard Barnes <rbarnes@umn.edu>
2018-02-16 01:40:59 -0500

    Add combined figure

--- tests/analysis.R
+++ tests/analysis.R
@@ -9,20 +9,20 @@ library(ggplot2)
 #######################
 #SERIAL ALGORITHMS
 
-df <- read.table('z_serial_benchmarks.tbl', header=TRUE, sep="|")
+dfserial <- read.table('z_serial_benchmarks.tbl', header=TRUE, sep="|")
 
-df <- df %>% select(-Rep,-host,-hash)                          %>% 
+dfserial <- dfserial %>% select(-Rep,-host,-hash)                          %>% 
              mutate(Algorithm=gsub('FastScape_','',Algorithm)) 
 
-widedf <- df
+widedf <- dfserial
 
-df <- melt(df, id.vars=c("Algorithm", "Size", "Steps")) %>%
+dfserial <- melt(dfserial, id.vars=c("Algorithm", "Size", "Steps")) %>%
       mutate(variable=gsub('Step(.)','Step \\1:',gsub('_',' ',variable)))
 
-df <- ddply(df, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable) %>% mutate(mean=mean/1e6, sd=sd/1e6)
-df$variable <- factor(df$variable, levels = rev(sort(as.character(unique(df$variable)))))
+dfserial <- ddply(dfserial, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable) %>% mutate(mean=mean/1e6, sd=sd/1e6)
+dfserial$variable <- factor(dfserial$variable, levels = rev(sort(as.character(unique(dfserial$variable)))))
 
-temp <- df %>% filter(Size==1000 | Size==10000) %>% filter(variable!="Total calculation time")
+temp <- dfserial %>% filter(Size==1000 | Size==10000) %>% filter(variable!="Total calculation time")
 p<-ggplot(temp, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_x") + coord_flip() + xlab("") + ylab("Wall-time (s)") + theme_grey(base_size=6) + theme(legend.position="bottom", legend.margin=margin(t = -0.3, unit='cm')) + guides(fill = guide_legend(keywidth=0.4, keyheight=0.4))
 ggsave(filename="img_serial_comparison.pdf", plot=p, units="in", width=3, height=1.5)
 
@@ -42,21 +42,21 @@ mean(erosion_per$per)
 #SIMPLE PARALLEL ALGORITHMS
 
 
-df <- read.table('z_simple_parallel_comet.tbl', header=TRUE, sep="|")
+df_p <- read.table('z_simple_parallel_comet.tbl', header=TRUE, sep="|")
 
-df <- df %>% select(-Rep,-host,-hash,-Random_seed)                                     %>% 
+df_p <- df_p %>% select(-Rep,-host,-hash,-Random_seed)                                     %>% 
              mutate(Algorithm=gsub('FastScape_','',Algorithm))
 
-widedf <- df
+widedf <- df_p
 
-df <- melt(df, id.vars=c("Algorithm", "Size", "Steps")) %>%
+df_p <- melt(df_p, id.vars=c("Algorithm", "Size", "Steps")) %>%
       mutate(variable=gsub('Step(.)','Step \\1:',gsub('_',' ',variable)))
 
-df <- ddply(df, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable) %>% mutate(mean=mean/1e6, sd=sd/1e6)
-df$variable <- factor(df$variable, levels = rev(sort(as.character(unique(df$variable)))))
+df_p <- ddply(df_p, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable) %>% mutate(mean=mean/1e6, sd=sd/1e6)
+df_p$variable <- factor(df_p$variable, levels = rev(sort(as.character(unique(df_p$variable)))))
 
-#ggplot(df, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_x") + coord_flip() + xlab("") + ylab("Wall-time (s)")
-temp <- df %>% filter(Size==1000 | Size==10000) %>% filter(variable!="Total calculation time")
+#ggplot(df_p, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_x") + coord_flip() + xlab("") + ylab("Wall-time (s)")
+temp <- df_p %>% filter(Size==1000 | Size==10000) %>% filter(variable!="Total calculation time")
 p<-ggplot(temp, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_x") + coord_flip() + xlab("") + ylab("Wall-time (s)") + theme_grey(base_size=6) + theme(legend.position="bottom", legend.margin=margin(t = -0.3, unit='cm')) + guides(fill = guide_legend(keywidth=0.4, keyheight=0.4))
 ggsave(filename="img_simple_parallel_comparison.pdf", plot=p, units="in", width=3, height=1.5)
 
@@ -78,21 +78,21 @@ mean(erosion_per$per)
 #*+PI.exe : Improved Parallelism
 ################################
 
-df <- read.table('z_parallel_improved_comet.tbl', header=TRUE, sep="|")
+df_pi <- read.table('z_parallel_improved_comet.tbl', header=TRUE, sep="|")
 
-df <- df %>% select(-Rep,-host,-hash,-Random_seed)                                     %>% 
+df_pi <- df_pi %>% select(-Rep,-host,-hash,-Random_seed)                                     %>% 
              mutate(Algorithm=gsub('FastScape_','',Algorithm))
 
-widedf <- df
+widedf <- df_pi
 
-df <- melt(df, id.vars=c("Algorithm", "Size", "Steps")) %>%
+df_pi <- melt(df_pi, id.vars=c("Algorithm", "Size", "Steps")) %>%
       mutate(variable=gsub('Step(.)','Step \\1:',gsub('_',' ',variable)))
 
-df <- ddply(df, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable) %>% mutate(mean=mean/1e6, sd=sd/1e6)
-df$variable <- factor(df$variable, levels = rev(sort(as.character(unique(df$variable)))))
+df_pi <- ddply(df_pi, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable) %>% mutate(mean=mean/1e6, sd=sd/1e6)
+df_pi$variable <- factor(df_pi$variable, levels = rev(sort(as.character(unique(df_pi$variable)))))
 
-#ggplot(df, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_x") + coord_flip() + xlab("") + ylab("Wall-time (s)")
-temp <- df %>% filter(Size==1000 | Size==10000) %>% filter(variable!="Total calculation time")
+#ggplot(df_pi, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_x") + coord_flip() + xlab("") + ylab("Wall-time (s)")
+temp <- df_pi %>% filter(Size==1000 | Size==10000) %>% filter(variable!="Total calculation time")
 p<-ggplot(temp, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_x") + coord_flip() + xlab("") + ylab("Wall-time (s)") + theme_grey(base_size=6) + theme(legend.position="bottom", legend.margin=margin(t = -0.3, unit='cm')) + guides(fill = guide_legend(keywidth=0.4, keyheight=0.4))
 ggsave(filename="img_parallel_improved_comparison.pdf", plot=p, units="in", width=3, height=1.5)
 
@@ -112,21 +112,21 @@ mean(erosion_per$per)
 #*+PQ.exe : Separated parallelism
 #################################
 
-df <- read.table('z_parallel_sep_thread_comet.tbl', header=TRUE, sep="|")
+df_pq <- read.table('z_parallel_sep_thread_comet.tbl', header=TRUE, sep="|")
 
-df <- df %>% select(-Rep,-host,-hash,-Random_seed)                                     %>% 
+df_pq <- df_pq %>% select(-Rep,-host,-hash,-Random_seed)                                     %>% 
              mutate(Algorithm=gsub('FastScape_','',Algorithm))
 
-widedf <- df
+widedf <- df_pq
 
-df <- melt(df, id.vars=c("Algorithm", "Size", "Steps")) %>%
+df_pq <- melt(df_pq, id.vars=c("Algorithm", "Size", "Steps")) %>%
       mutate(variable=gsub('Step(.)','Step \\1:',gsub('_',' ',variable)))
 
-df <- ddply(df, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable) %>% mutate(mean=mean/1e6, sd=sd/1e6)
-df$variable <- factor(df$variable, levels = rev(sort(as.character(unique(df$variable)))))
+df_pq <- ddply(df_pq, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable) %>% mutate(mean=mean/1e6, sd=sd/1e6)
+df_pq$variable <- factor(df_pq$variable, levels = rev(sort(as.character(unique(df_pq$variable)))))
 
-#ggplot(df, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_x") + coord_flip() + xlab("") + ylab("Wall-time (s)") + scale_fill_manual(values=c('#00BFC4'), guide = guide_legend()) + theme(legend.position="bottom")
-temp <- df %>% filter(Size==1000 | Size==10000) %>% filter(variable!="Total calculation time")
+#ggplot(df_pq, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_x") + coord_flip() + xlab("") + ylab("Wall-time (s)") + scale_fill_manual(values=c('#00BFC4'), guide = guide_legend()) + theme(legend.position="bottom")
+temp <- df_pq %>% filter(Size==1000 | Size==10000) %>% filter(variable!="Total calculation time")
 p<-ggplot(temp, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_x") + coord_flip() + xlab("") + ylab("Wall-time (s)") + scale_fill_manual(values=c('#00BFC4'), guide = guide_legend()) + theme_grey(base_size=6) + theme(legend.position="bottom", legend.margin=margin(t = -0.3, unit='cm')) + guides(fill = guide_legend(keywidth=0.4, keyheight=0.4))
 ggsave(filename="img_parallel_sep_thread_comet.pdf", plot=p, units="in", width=3, height=1.5)
 
@@ -144,21 +144,21 @@ mean(erosion_per$per)
 #*+GPU.exe : GPU
 #################################
 
-df <- read.table('z_gpu_summitdev.tbl', header=TRUE, sep="|")
+df_gpu <- read.table('z_gpu_summitdev.tbl', header=TRUE, sep="|")
 
-df <- df %>% select(-Rep,-host,-hash,-Random_seed)                                     %>% 
+df_gpu <- df_gpu %>% select(-Rep,-host,-hash,-Random_seed)                                     %>% 
              mutate(Algorithm=gsub('FastScape_','',Algorithm))
 
-widedf <- df
+widedf <- df_gpu
 
-df <- melt(df, id.vars=c("Algorithm", "Size", "Steps")) %>%
+df_gpu <- melt(df_gpu, id.vars=c("Algorithm", "Size", "Steps")) %>%
       mutate(variable=gsub('Step(.)','Step \\1:',gsub('_',' ',variable)))
 
-df <- ddply(df, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable) %>% mutate(mean=mean/1e6, sd=sd/1e6)
-df$variable <- factor(df$variable, levels = rev(sort(as.character(unique(df$variable)))))
+df_gpu <- ddply(df_gpu, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable) %>% mutate(mean=mean/1e6, sd=sd/1e6)
+df_gpu$variable <- factor(df_gpu$variable, levels = rev(sort(as.character(unique(df_gpu$variable)))))
 
-#ggplot(df, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_x") + coord_flip() + xlab("") + ylab("Wall-time (s)") + scale_fill_manual(values=c('#00BFC4'), guide = guide_legend()) + theme(legend.position="bottom")
-temp <- df %>% filter(Size==1000 | Size==10000) %>% filter(variable!="Total calculation time")
+#ggplot(df_gpu, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_x") + coord_flip() + xlab("") + ylab("Wall-time (s)") + scale_fill_manual(values=c('#00BFC4'), guide = guide_legend()) + theme(legend.position="bottom")
+temp <- df_gpu %>% filter(Size==1000 | Size==10000) %>% filter(variable!="Total calculation time")
 p<-ggplot(temp, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_x") + coord_flip() + xlab("") + ylab("Wall-time (s)") + scale_fill_manual(values=c('#00BFC4'), guide = guide_legend()) + theme_grey(base_size=6) + theme(legend.position="bottom", legend.margin=margin(t = -0.3, unit='cm')) + guides(fill = guide_legend(keywidth=0.4, keyheight=0.4))
 ggsave(filename="img_gpu_summitdev.pdf", plot=p, units="in", width=3, height=1.5)
 
@@ -174,19 +174,19 @@ mean(erosion_per$per)
 #*+GPU.exe : GPU Scaling
 #################################
 
-df <- read.table('z_gpu_scaling_summitdev.tbl', header=TRUE, sep="|")
+df_gpu_scaling <- read.table('z_gpu_scaling_summitdev.tbl', header=TRUE, sep="|")
 
-df <- df %>% select(-Rep,-host,-hash,-Random_seed)                                     %>% 
+df_gpu_scaling <- df_gpu_scaling %>% select(-Rep,-host,-hash,-Random_seed)                                     %>% 
              mutate(Algorithm=gsub('FastScape_','',Algorithm))
 
-df <- melt(df, id.vars=c("Algorithm", "Size", "Steps")) %>%
+df_gpu_scaling <- melt(df_gpu_scaling, id.vars=c("Algorithm", "Size", "Steps")) %>%
       mutate(variable=gsub('Step(.)','Step \\1:',gsub('_',' ',variable)))
 
-df <- ddply(df, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable) %>% mutate(mean=mean/1e6, sd=sd/1e6)
-df$variable <- factor(df$variable, levels = rev(sort(as.character(unique(df$variable)))))
+df_gpu_scaling <- ddply(df_gpu_scaling, c("Algorithm", "Size", "Steps", "variable"), summarise, mean = mean(value), sd = sd(value)) %>% arrange(Algorithm,variable) %>% mutate(mean=mean/1e6, sd=sd/1e6)
+df_gpu_scaling$variable <- factor(df_gpu_scaling$variable, levels = rev(sort(as.character(unique(df_gpu_scaling$variable)))))
 
-#ggplot(df, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_x") + coord_flip() + xlab("") + ylab("Wall-time (s)") + scale_fill_manual(values=c('#00BFC4'), guide = guide_legend()) + theme(legend.position="bottom")
-temp <- df %>% filter(variable=="Overall")
+#ggplot(df_gpu_scaling, aes(x=variable, y=mean, fill=Algorithm)) + geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_wrap(~Size, scales="free_x") + coord_flip() + xlab("") + ylab("Wall-time (s)") + scale_fill_manual(values=c('#00BFC4'), guide = guide_legend()) + theme(legend.position="bottom")
+temp <- df_gpu_scaling %>% filter(variable=="Overall")
 
 p<-ggplot(temp, aes(x=Size**2, y=mean, color=Algorithm)) + 
      #geom_rect(aes(xmin = 0,    xmax = 400, ymin = min(temp$mean), ymax = Inf), fill = "pink", alpha = 0.01, color=NA) +
@@ -225,3 +225,37 @@ lm(log10(lmdat$mean) ~ log10(lmdat$Size**2))
 
 lmdat <- temp %>% filter(2500<=Size)
 lm(log10(lmdat$mean) ~ log10(lmdat$Size**2))
+
+
+
+
+
+
+############################
+#Overall timing comparison
+
+
+
+dfc <- rbind(dfserial,df_p,df_pi,df_pq,df_gpu)
+
+temp <- dfc %>% filter(variable=="Overall" & (Size==1000 | Size==10000)) %>% arrange(desc(mean))
+
+temp$Algorithm      <- factor(temp$Algorithm, levels = c(" RB+GPU ", " RB+PQ ", " RB+PI  ", " B&W+PI ", " RB+P  ",  " B&W+P ",  " RB  ", " B&W " ))
+temp$Implementation <- temp$Algorithm
+temp$Algorithm      <- gsub(' ','',gsub('\\+.*','',temp$Algorithm))
+
+
+
+p<-ggplot(temp, aes(x=Implementation, y=mean, fill=Algorithm)) + 
+   facet_wrap(~Size) + 
+   geom_bar(stat="identity", position="dodge") +
+   theme(axis.text.x=element_text(angle=45, hjust=1)) + 
+   facet_wrap(~Size, scales="free_x") + 
+   coord_flip() + 
+   xlab("") + 
+   ylab("Wall-time (s)") +
+   # scale_fill_manual(values=c('#00BFC4'), guide = guide_legend()) + 
+   theme_grey(base_size=6) +
+   theme(legend.position="bottom", legend.margin=margin(t = -0.3, unit='cm')) + 
+   guides(fill = guide_legend(keywidth=0.4, keyheight=0.4))
+ggsave(filename="img_timing_compare.pdf", plot=p, units="in", width=3, height=1.5)
