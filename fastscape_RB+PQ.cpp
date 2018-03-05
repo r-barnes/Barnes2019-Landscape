f1ac64fdc8576abb2c2059750221bf87f2195197
Richard Barnes <rbarnes@umn.edu>
2018-03-05 13:27:18 -0500

    Switched to vectors. Increased commenting.

--- fastscape_RB+PQ.cpp
+++ fastscape_RB+PQ.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <cassert>
 #include <cmath>
 #include <cstdlib>
@@ -26,7 +27,7 @@
 ///format as it will have a smaller file size and, thus, save quicker.
 void PrintDEM(
   const std::string filename, 
-  const double *const h,
+  const std::vector<double>& h,
   const int width,
   const int height
 ){
@@ -88,24 +89,17 @@ class FastScape_RBPQ {
   //0   4
   //7 6 5
 
-  double *h;        //Digital elevation model (height)
-  double *accum;    //Flow accumulation at each point
-  int    *rec;      //Direction of receiving cell
-  int    *donor;    //Indices of a cell's donor cells
-  int    *ndon;     //How many donors a cell has
-  int    *stack;    //Indices of cells in the order they should be processed
-  int    nshift[8]; //Offset from a focal cell's index to its neighbours in terms of flat indexing
-
-  //A level is a set of cells which can all be processed simultaneously.
-  //Topologically, cells within a level are neither descendents or ancestors of
-  //each other in a topological sorting, but are the same number of steps from
-  //the edge of the dataset.
-  int    *levels;   //Indices of locations in stack where a level begins and ends
-  int    nlevel;    //Number of levels used
+  std::vector<double> h;        //Digital elevation model (height)
+  std::vector<double> accum;    //Flow accumulation at each point
+  std::vector<int>    rec;      //Direction of receiving cell
+  std::vector<int>    donor;    //Indices of a cell's donor cells
+  std::vector<int>    ndon;     //How many donors a cell has
+  std::array<int,8>   nshift;   //Offset from a focal cell's index to its neighbours in terms of flat indexing
 
   int stack_width;  //Number of cells allowed in the stack
   int level_width;  //Number of cells allowed in a level
 
+  //Timers for keeping track of how long each part of the code takes
   CumulativeTimer Tmr_Step1_Initialize;
   CumulativeTimer Tmr_Step2_DetermineReceivers;
   CumulativeTimer Tmr_Step3_DetermineDonors;
@@ -151,19 +145,12 @@ class FastScape_RBPQ {
     height = height0;
     size   = width*height;
 
-    h      = new double[size];   //Memory for terrain height
+    h.resize(size);   //Memory for terrain height
 
     GenerateRandomTerrain();     //Could replace this with custom initializer
 
     Tmr_Step1_Initialize.stop();
     Tmr_Overall.stop();
-  }
-
-
-
-  ///Destructor: ensures that `h` is freed when the class goes out of scope
-  ~FastScape_RBPQ(){
-    delete[] h;
   }
 
 
@@ -174,29 +161,35 @@ class FastScape_RBPQ {
   ///focal cell by the steppest gradient. If there is no local gradient, than
   ///the special value NO_FLOW is assigned.
   void ComputeReceivers(){
-    //! computing receiver array
-    #pragma omp for simd collapse(2)
+    //Edge cells do not have receivers because they do not distribute their flow
+    //to anywhere.
+
+    #pragma omp for collapse(2)
     for(int y=2;y<height-2;y++)
     for(int x=2;x<width-2;x++){
       const int c      = y*width+x;
 
       //The slope must be greater than zero for there to be downhill flow;
-      //otherwise, the cell is marekd NO_FLOW
-      double max_slope = 0;
-      int    max_n     = NO_FLOW;
+      //otherwise, the cell is marked NO_FLOW.
+      double max_slope = 0;        //Maximum slope seen so far amongst neighbours
+      int    max_n     = NO_FLOW;  //Direction of neighbour which had maximum slope to focal cell
 
+      //Loop over neighbours
       for(int n=0;n<8;n++){
-        double slope = (h[c] - h[c+nshift[n]])/dr[n];
-        if(slope>max_slope){
-          max_slope = slope;
-          max_n     = n;
+        const double slope = (h[c] - h[c+nshift[n]])/dr[n]; //Slope to neighbour n
+        if(slope>max_slope){    //Is this the steepest slope we've seen?
+          max_slope = slope;    //If so, make a note of the slope
+          max_n     = n;        //And which cell it came from
         }
       }
-      rec[c] = max_n;
+      rec[c] = max_n;           //Having considered all neighbours, this is the steepest
     }    
   }
 
 
+
+  ///The donors of a focal cell are the neighbours from which it receives flow.
+  ///Here, we identify those neighbours by inverting the Receivers array.
   void ComputeDonors(){
     //The B&W method of developing the donor array has each focal cell F inform
     //its receiving cell R that F is a donor of R. Unfortunately, parallelizing
@@ -207,13 +200,19 @@ class FastScape_RBPQ {
     //Instead, we invert the operation. Each focal cell now examines its
     //neighbours to see if it receives from them. Each focal cell is then
     //guaranteed to have sole write-access to its location in the donor array.
-    #pragma omp for simd collapse(2) schedule(static) nowait
+
+    //Remember, the outermost ring of cells is a convenience halo, so we don't
+    //calculate donors for it.
+
+    #pragma omp for collapse(2) schedule(static) nowait
     for(int y=1;y<height-1;y++)
     for(int x=1;x<width-1;x++){
       const int c = y*width+x;
-      ndon[c] = 0;
+      ndon[c] = 0; //Cell has no donor neighbours we know about
       for(int ni=0;ni<8;ni++){
         const int n = c+nshift[ni];
+        //If the neighbour has a receiving cell and that receiving cell is
+        //the current focal cell c
         if(rec[n]!=NO_FLOW && n+nshift[rec[n]]==c){
           donor[8*c+ndon[c]] = n;
           ndon[c]++;
@@ -222,9 +221,17 @@ class FastScape_RBPQ {
     }
   }
 
+
+
+  ///Cells must be ordered so that they can be traversed such that higher cells
+  ///are processed before their lower neighbouring cells. This method creates
+  ///such an order. It also produces a list of "levels": cells which are,
+  ///topologically, neither higher nor lower than each other. Cells in the same
+  ///level can all be processed simultaneously without having to worry about
+  ///race conditions.
   void GenerateOrder(
-    int *const stack,
-    int *const levels,
+    std::vector<int>& stack,
+    std::vector<int>& levels,
     int &nlevel
   ){
     int nstack = 0;
@@ -261,39 +268,48 @@ class FastScape_RBPQ {
         assert(nstack<stack_width);
       }
     }
-    //Last cell of this level
-    levels[nlevel++] = nstack;              
+    levels[nlevel++] = nstack; //Last cell of this level
     assert(nlevel<level_width); 
 
-    int level_bottom = -1;
-    int level_top    = 0;
+    //Start with level_bottom=-1 so we get into the loop, it is immediately
+    //replaced by level_top.
+    int level_bottom = -1;         //First cell of the current level
+    int level_top    =  0;         //Last cell of the current level
 
     #pragma omp barrier //Must ensure ComputeDonors is done before we start the following
 
-    while(level_bottom<level_top){
-      level_bottom = level_top;
-      level_top    = nstack;
+    while(level_bottom<level_top){ //Ensure we parse all the cells
+      level_bottom = level_top;    //The top of the previous level we considered is the bottom of the current level
+      level_top    = nstack;       //The new top is the end of the stack (last cell added from the previous level)
       for(int si=level_bottom;si<level_top;si++){
         const auto c = stack[si];
+        //Load donating neighbours of focal cell into the stack
         for(int k=0;k<ndon[c];k++){
-          const auto n    = donor[8*c+k];
+          const auto n = donor[8*c+k];
           stack[nstack++] = n;              
-          assert(nstack<stack_width); //TODO `<=` or `<` ?
+          assert(nstack<stack_width);
         }
       }
 
-      levels[nlevel++] = nstack; //Starting a new level      
+      levels[nlevel++] = nstack; //Start a new level
     }
 
     //End condition for the loop places two identical entries
     //at the end of the stack. Remove one.
     nlevel--;
+
+    assert(levels[nlevel-1]==nstack);
   }
 
 
+
+  ///Compute the flow accumulation for each cell: the number of cells whose flow
+  ///ultimately passes through the focal cell multiplied by the area of each
+  ///cell. Each cell could also have its own weighting based on, say, average
+  ///rainfall.
   void ComputeFlowAcc(
-    const int *const stack,
-    const int *const levels,
+    const std::vector<int>& stack,
+    const std::vector<int>& levels,
     const int &nlevel
   ){
     for(int i=levels[0];i<levels[nlevel-1];i++){
@@ -301,9 +317,21 @@ class FastScape_RBPQ {
       accum[c] = cell_area;
     }
 
+    //Highly-elevated cells pass their flow to less elevated neighbour cells.
+    //The queue is ordered so that higher cells are keyed to higher indices in
+    //the queue; therefore, parsing the queue in reverse ensures that fluid
+    //flows downhill.
+
+    //We can process the cells in each level in parallel. To prevent race
+    //conditions, each focal cell figures out what contirbutions it receives
+    //from its neighbours.
+
+    //`nlevel-1` is the upper bound of the stack.
+    //`nlevel-2` through `nlevel-1` are the cells which have no higher neighbours (top of the watershed)
+    //`nlevel-3` through `nlevel-2` are the first set of cells with higher neighbours, so this is where we start
     for(int li=nlevel-3;li>=1;li--){
-      const int lvlstart = levels[li];
-      const int lvlend   = levels[li+1];
+      const int lvlstart = levels[li];      //Starting index of level in stack
+      const int lvlend   = levels[li+1];    //Ending index of level in stack
       for(int si=lvlstart;si<lvlend;si++){
         const int c = stack[si];
         for(int k=0;k<ndon[c];k++){
@@ -315,11 +343,19 @@ class FastScape_RBPQ {
   }
 
 
+  ///Raise each cell in the landscape by some amount, otherwise it wil get worn
+  ///flat (in this model, with these settings)
   void AddUplift(
-    const int *const stack,
-    const int *const levels,
+    const std::vector<int>& stack,
+    const std::vector<int>& levels,
     const int &nlevel
   ){
+    //We exclude two exterior rings of cells in this example. The outermost ring
+    //(the edges of the dataset) allows us to ignore the edges of the dataset,
+    //the second-most outer ring (the cells bordering the edge cells of the
+    //dataset) are fixed to a specified height in this model. All other cells
+    //have heights which actively change and they are altered here.
+
     //Start at levels[1] so we don't elevate the outer edge
     #pragma omp simd
     for(int i=levels[1];i<levels[nlevel-1];i++){
@@ -329,48 +365,71 @@ class FastScape_RBPQ {
   }
 
 
+
+  ///Decrease he height of cells according to the stream power equation; that
+  ///is, based on a constant K, flow accumulation A, the local slope between
+  ///the cell and its receiving neighbour, and some judiciously-chosen constants
+  ///m and n.
+  ///    h_next = h_current - K*dt*(A^m)*(Slope)^n
+  ///We solve this equation implicitly to preserve accuracy
   void Erode(
-    const int *const stack,
-    const int *const levels,
+    const std::vector<int>& stack,
+    const std::vector<int>& levels,
     const int nlevel
   ){
+    //The cells in each level can be processed in parallel, so we loop over
+    //levels starting from the lower-most (the one closest to the NO_FLOW cells)
+
     //#pragma omp parallel default(none)
     for(int li=2;li<nlevel-1;li++){
       const int lvlstart = levels[li];
       const int lvlend   = levels[li+1];
       #pragma omp simd
       for(int si=lvlstart;si<lvlend;si++){
-        const int c = stack[si];          //Cell from which flow originates
-        const int n = c+nshift[rec[c]];   //Cell receiving the flow
+        const int c = stack[si];         //Cell from which flow originates
+        const int n = c+nshift[rec[c]];  //Cell receiving the flow
 
         const double length = dr[rec[c]];
+        //`fact` contains a set of values which are constant throughout the integration
         const double fact   = keq*dt*std::pow(accum[c],meq)/std::pow(length,neq);
-        const double h0     = h[c];        //Elevation of focal cell
-        const double hn     = h[n];        //Elevation of neighbouring (receiving, lower) cell
-        double hnew         = h0;          //Current updated value of focal cell
-        double hp           = h0;          //Previous updated value of focal cell
-        double diff         = 2*tol;       //Difference between current and previous updated values
-        while(std::abs(diff)>tol){
+        const double h0     = h[c];      //Elevation of focal cell
+        const double hn     = h[n];      //Elevation of neighbouring (receiving, lower) cell
+        double hnew         = h0;        //Current updated value of focal cell
+        double hp           = h0;        //Previous updated value of focal cell
+        double diff         = 2*tol;     //Difference between current and previous updated values
+        while(std::abs(diff)>tol){       //Newton-Rhapson method (run until subsequent values differ by less than a tolerance, which can be set to any desired precision)
           hnew -= (hnew-h0+fact*std::pow(hnew-hn,neq))/(1.+fact*neq*std::pow(hnew-hn,neq-1));
-          diff  = hnew - hp;
-          hp    = hnew;
+          diff  = hnew - hp;             //Difference between previous and current value of the iteration
+          hp    = hnew;                  //Update previous value to new value
         }
-        h[c] = hnew;
+        h[c] = hnew;                     //Update value in array
       }
     }
   }
 
 
  public:
+
+  ///Run the model forward for a specified number of timesteps. No new
+  ///initialization is done. This allows the model to be stopped, the terrain
+  ///altered, and the model continued. For space-efficiency, a number of
+  ///temporary arrays are created each time this is run, so repeatedly running
+  ///this function for the same model will likely not be performant due to
+  ///reallocations. If that is your use case, you'll want to modify your code
+  ///appropriately.
   void run(const int nstep){
     Tmr_Overall.start();
 
     Tmr_Step1_Initialize.start();
 
-    accum  = new double[size];
-    rec    = new int[size];
-    ndon   = new int[size];
-    donor  = new int[8*size];
+    //TODO: Make smaller, explain max
+    stack_width = std::max(300000,5*size/omp_get_max_threads()); //Number of stack entries available to each thread
+    level_width = std::max(1000,size/omp_get_max_threads());     //Number of level entries available to each thread
+
+    accum.resize(  size);  //Stores flow accumulation
+    rec.resize  (  size);  //Array of Receiver directions
+    ndon.resize (  size);  //Number of donors each cell has
+    donor.resize(8*size);  //Array listing the donors of each cell (up to 8 for a rectangular grid)
 
     //! initializing rec
     #pragma omp parallel for
@@ -381,13 +440,15 @@ class FastScape_RBPQ {
     for(int i=0;i<size;i++)
       ndon[i] = 0;
 
-    //TODO: Make smaller, explain max
-    stack_width = std::max(300000,5*size/omp_get_max_threads()); //Number of stack entries available to each thread
-    level_width = std::max(1000,size/omp_get_max_threads());   //Number of level entries available to each thread
 
     #pragma omp parallel
     {
-      int *stack  = new int[stack_width]; //Indices of cells in the order they should be processed
+      std::vector<int> stack(stack_width); //Indices of cells in the order they should be processed
+
+      //A level is a set of cells which can all be processed simultaneously.
+      //Topologically, cells within a level are neither descendents or ancestors
+      //of each other in a topological sorting, but are the same number of steps
+      //from the edge of the dataset.
 
       //It's difficult to know how much memory should be allocated for levels. For
       //a square DEM with isotropic dispersion this is approximately sqrt(E/2). A
@@ -395,7 +456,7 @@ class FastScape_RBPQ {
       //levels. A tortorously sinuous river may have up to E*E levels. We
       //compromise and choose a number of levels equal to the perimiter because
       //why not?
-      int *levels = new int[level_width]; //TODO
+      std::vector<int> levels(level_width);
       int  nlevel = 0;
 
       Tmr_Step1_Initialize.stop();
@@ -410,12 +471,9 @@ class FastScape_RBPQ {
         #pragma omp barrier //Ensure threads synchronize after erosion so we calculate receivers correctly
 
         #pragma omp master
-        if( step%20==0 )
+        if( step%20==0 ) //Show progress
           std::cout<<"p Step = "<<step<<std::endl;
       }
-
-      delete[] stack;
-      delete[] levels;
     }
 
     Tmr_Overall.stop();
@@ -429,18 +487,19 @@ class FastScape_RBPQ {
     std::cout<<"t Step7: Erosion            = "<<std::setw(15)<<Tmr_Step7_Erosion.elapsed()            <<" microseconds"<<std::endl;              
     std::cout<<"t Overall                   = "<<std::setw(15)<<Tmr_Overall.elapsed()                  <<" microseconds"<<std::endl;        
 
-    delete[] accum;
-    delete[] rec;
-    delete[] ndon;
-    delete[] donor;
+    //Free up memory, except for the resulting landscape height field prior to
+    //exiting so that unnecessary space is not used when the model is not being
+    //run.
+    accum .clear();   accum .shrink_to_fit();
+    rec   .clear();   rec   .shrink_to_fit();
+    ndon  .clear();   ndon  .shrink_to_fit();
+    donor .clear();   donor .shrink_to_fit();
   }
 
-      // std::cerr<<"Levels: ";
-      // for(auto &l: levels)
-      //   std::cerr<<l<<" ";
-      // std::cerr<<std::endl;
 
-  double* getH() const {
+
+  ///Returns a pointer to the data so that it can be copied, printed, &c.
+  std::vector<double>& getH() {
     return h;
   }
 };
@@ -452,6 +511,7 @@ class FastScape_RBPQ {
 
 
 int main(int argc, char **argv){
+  //Enable this to stop the program if a floating-point exception happens
   //feenableexcept(FE_ALL_EXCEPT);
 
   if(argc!=5){
@@ -459,23 +519,30 @@ int main(int argc, char **argv){
     return -1;
   }
 
-  seed_rand(std::stoul(argv[4]));
+  const int         width       = std::stoi (argv[1]);
+  const int         height      = std::stoi (argv[1]);
+  const int         nstep       = std::stoi (argv[2]);
+  const std::string output_name =            argv[3] ;
+  const auto        rand_seed   = std::stoul(argv[4]);
 
-  std::cout<<"A FastScape RB+PQ"<<std::endl;
+  seed_rand(rand_seed);
+
+  //Uses the RichDEM machine-readable line prefixes
+  //Name of algorithm
+  std::cout<<"A FastScape RB+PQ"<<std::endl;                
+  //Citation for algorithm
   std::cout<<"C Richard Barnes TODO"<<std::endl;
+  //Git hash of code used to produce outputs of algorithm
   std::cout<<"h git_hash    = "<<GIT_HASH<<std::endl;
-  std::cout<<"m Random seed = "<<argv[4]<<std::endl;
-
-  const int width  = std::stoi(argv[1]);
-  const int height = std::stoi(argv[1]);
-  const int nstep  = std::stoi(argv[2]);
+  //Random seed used to produce outputs
+  std::cout<<"m Random seed = "<<rand_seed<<std::endl;
 
   CumulativeTimer tmr(true);
   FastScape_RBPQ tm(width,height);
   tm.run(nstep);
   std::cout<<"t Total calculation time    = "<<std::setw(15)<<tmr.elapsed()<<" microseconds"<<std::endl;
 
-  PrintDEM(argv[3], tm.getH(), width, height);
+  PrintDEM(output_name, tm.getH(), width, height);
 
   return 0;
 }
